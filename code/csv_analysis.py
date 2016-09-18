355293aec358d6027726e268c349364bb7d42690
Frederic Lafrance <lafrancef@gmail.com>
2016-09-18 14:28:21 +0200

    Speed improvements to shape analysis

--- code/csv_analysis.py
+++ code/csv_analysis.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 def shape_distance(a, b):
     '''
     Determines the 'distance' of the shape of two sequences of data. This
@@ -9,47 +12,53 @@ def shape_distance(a, b):
     Therefore, a measure of 0 means that the sequences have the same shape.
     '''
 
-    # We'd like to keep track of the last element as well.
-    # If we have the sequence [1.0], that should be treated differently
-    # from [0.1]
-    a.append(0)
-    b.append(0)
-
-    deltas_a = [a[i] - a[i-1] for i in range(1, len(a))]
-    deltas_b = [b[i] - b[i-1] for i in range(1, len(b))]
-
-    if len(deltas_a) < len(deltas_b):
-        deltas_a.extend([0] * (len(deltas_b) - len(deltas_a)))
-    else:
-        deltas_b.extend([0] * (len(deltas_a) - len(deltas_b)))
-
     measure = 0.0
-    for i in range(max(len(deltas_a), len(deltas_b))):
-        x_a = deltas_a[i]
-        x_b = deltas_b[i]
+    for i in range(len(a)):
+        x_a = a[i]
+        x_b = b[i]
 
-        measure += (x_a - x_b) * (x_a - x_b)
+        measure += (a[i] - b[i]) * (a[i] - b[i])
 
     return measure
 
+def make_deltas(seqs):
+    withZeros = [x + [0] for x in seqs]
+    deltas = [[x[i] - x[i-1] for i in range(1, len(x))] for x in withZeros]
+    maxLen = max([len(x) for x in deltas])
+    for i, seq in enumerate(deltas):
+        deltas[i] = seq + ([0] * (maxLen - len(seq)))
+
+    return deltas
+
+
 def cluster(seqs, threshold):
 
+    last = 0
     clusters = [] # clusters[i] is a two element tuple of sequences and sets
-    for i, seq in enumerate(seqs):
+    deltas = make_deltas(seqs)
+    print('Clustering', len(seqs), 'sequences', file=sys.stderr)
+
+    for i, seq in enumerate(deltas):
         min_clu = -1
         min_dist = 1 #???
 
         for j, clu in enumerate(clusters):
-            dist = shape_distance(seq, seqs[clu[0]])
+            dist = shape_distance(seq, deltas[clu[0]])
             if dist < min_dist or min_clu == -1:
                 min_clu = j
                 min_dist = dist
 
         if min_clu != -1 and min_dist < threshold:
-            clusters[min_clu][1].append(i)
+            clusters[min_clu][1].add(i)
         else:
-            new_clu = (i, [i])
+            new_clu = (i, set([i]))
             clusters.append(new_clu)
+
+        if i % 1000 == 0:
+            print(i, 'clus:', len(clusters), 'time:',
+                    time.perf_counter() - last, file=sys.stderr)
+            last = time.perf_counter()
+
 
     return clusters
 
@@ -59,8 +68,13 @@ def print_clus(seqs, clus):
     for i, clu in enumerate(clus):
         print('\tClu', i, ':', len(clu[1]), 'elems, rep:', seqs[clu[0]])
         print('\t\tSample:', end=' ')
-        for elem in clu[1][:8]:
-            print(seqs[elem], end=',')
+
+        for _ in range(8):
+            if len(clu[1]) > 0:
+                print(seqs[clu[1].pop()], end=',')
+            else:
+                break
+
         print()
 
 def print_clu(seqs, clu):
@@ -77,4 +91,5 @@ if __name__ == '__main__':
         shapes.append([float(x) for x in sims])
         ln = hdl.readline()
 
-    print_clus(shapes, cluster(shapes, 0.08))
+    clus = cluster(shapes, 0.09)
+    print_clus(shapes, clus)
